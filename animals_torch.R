e4447b5d29e2bbf04a6790523edaff10fa4d8530
Roy Sanderson <roy.sanderson@newcastle.ac.uk>
2022-08-12 17:21:44 +0100

    Successfully added probabilities, auc and ROC plots
    Need to use exp to calculate probabilities as these are not output
    by luz or torch directly but as logits, unlike keras.

--- animals_torch.R
+++ animals_torch.R
@@ -131,7 +131,7 @@ net <- nn_module(
 )
 
 # Training the model -----------------------------------------------------------
-n_epochs <- 5
+n_epochs <- 10
 
 fitted <- net %>%
   setup(
@@ -143,14 +143,14 @@ fitted <- net %>%
   ) %>%
   fit(train_dl, epochs = n_epochs, valid_data = valid_dl)
 
-
-#pred <- torch_max(fitted(valid_ds$x_num, valid_ds$x_cat), dim = 2)[[2]] %>%
+# Make predictions
 pred <- predict(fitted, valid_ds) %>% 
   as_array() %>% 
   max.col()
 truth <- valid_ds$samples[[2]] %>%
   as.vector()
 
+# Calculate and display confusion matrix
 confusion <- bind_cols(pred = pred, truth = truth) %>%
   mutate(across(everything(), ~factor(.x, levels = 1:4, labels = class_names))) %>%
   conf_mat(truth, pred)
@@ -158,331 +158,25 @@ confusion <- bind_cols(pred = pred, truth = truth) %>%
 autoplot(confusion, type = "heatmap") + 
   scale_fill_distiller(palette = 2, direction = "reverse")
 
-#torch_manual_seed(123)
-#model <- net()
-#model$to(device = device)
-#criterion <- nn_cross_entropy_loss()
-#optimizer <- optim_sgd(model$parameters, lr = 0.05, momentum = 0.9)
-#
-#
-#scheduler <- optimizer %>% 
-#  lr_one_cycle(max_lr = 0.05, epochs = n_epochs, steps_per_epoch = train_dl$.length())
-#
-#train_batch <- function(b) {
-#  optimizer$zero_grad()
-#  output <- model(b[[1]])
-#  loss <- criterion(output, b[[2]])
-#  loss$backward()
-#  optimizer$step()
-#  scheduler$step()
-#  loss$item()
-#}
-#
-#valid_batch <- function(b) {
-#  output <- model(b[[1]])
-#  loss <- criterion(output, b[[2]])
-#  loss$item()
-#}
-#
-#for (epoch in 1:n_epochs) {
-#  model$train()
-#  train_losses <- c()
-#
-#  coro::loop(for (b in train_dl) {
-#    loss <- train_batch(b)
-#    train_losses <- c(train_losses, loss)
-#  })
-#
-#  model$eval()
-#  valid_losses <- c()
-#
-#  coro::loop(for (b in valid_dl) {
-#    loss <- valid_batch(b)
-#    valid_losses <- c(valid_losses, loss)
-#  })
-#
-#  cat(sprintf("\nLoss at epoch %d: training: %3f, validation: %3f\n", epoch, mean(train_losses), mean(valid_losses)))
-#}
+# Calculate and display various accuracy metrics and ROC plots
+# Above gives logits 
+# keras uses categorical cross entropy (gives probabilities)
+# torch uses nnf_cross_entropy which gives logits hence no activation
+# in last layer of model structure. Use exp to convert logits back to probs.
+pred2 <- exp(as.matrix(predict(fitted, valid_ds)))
+probs <- pred2 / rowSums(pred2)
+joint <- data.frame(as.factor(truth), probs, as.factor(pred))
+colnames(joint) <- c("truth", class_names, "pred")
+metrics(joint, truth, pred)
+precision(joint, truth, pred)
 
+joint %>%
+  mutate(Resample = 1) %>% 
+  group_by(Resample) %>%
+  roc_auc(truth, butterfly:spider)
 
-#
-#
-#model <- net()
-#model$to(device = device)
-#optimizer <- optim_adam(model$parameters)
-#
-#n_epochs <- 5
-#
-#for (epoch in 1:n_epochs) {
-#  l <- c()
-#  coro::loop(for (b in train_dl){
-#    optimizer$zero_grad()
-#    output <- model(b[[1]]$to(device = device))
-#    loss <- nnf_cross_entropy(output, b[[2]]$to(device = device))
-#    loss$backward()
-#    optimizer$step()
-#    l <- c(l, loss$item())
-#  })
-#  cat(sprintf("Loss at epoch %d: %3f\n", epoch, mean(l)))
-#}
-#
-#
-#
-# net <- nn_module(
-#   
-#   "simple-cnn",
-#   
-#   initialize = function() {
-#     # start with batch_size * channels * width * height
-#     # 32 * 3 * 250 * 250 = 6e+06
-#     # in_channels, out_channels, kernel_size, stride = 1, padding = 0
-#     self$conv1 <- nn_conv2d(3, 64, kernel_size = 3) #nn_conv2d(1, 32, 3)
-#     # Above converts to 32 * 32 * 249 * 249 image size ??!!
-#     self$conv2 <- nn_conv2d(64, 32, kernel_size = 3) #nn_conv2d(32, 64, 3)
-#     # Above converts to 32 * 64 * 248 * 248
-#     self$dropout1 <- nn_dropout2d(0.25)
-#     self$dropout2 <- nn_dropout2d(0.5)
-#     # The nnf_max_pool2d(2) below converts to
-#     # 32 * 64 * 124 * 124. 64 * 124 * 124 = 984064
-#     self$fc1 <- nn_linear(984064, 32) # nn_linear(9216, 128)
-#     self$fc2 <- nn_linear(32, out_features = 4) #nn_linear(128, 4)
-#    },
-#   
-#   forward = function(x) {
-#     x %>% 
-#       self$conv1() %>%
-#       nnf_relu() %>%
-#       self$conv2() %>%
-#       nnf_relu() %>%
-#       nnf_max_pool2d(2) %>%
-#       self$dropout1() %>%
-#       torch_flatten(start_dim = 2) %>%
-#       self$fc1() %>%
-#       nnf_relu() %>%
-#       self$dropout2() %>%
-#       self$fc2()     
-#   }
-# )
-#
-#
-## Training ----
-#
-#fitted <- net %>%
-#  setup(
-#    loss = nn_cross_entropy_loss(),
-#    optimizer = optim_adam,
-#    metrics = list(
-#      luz_metric_accuracy()
-#    )
-#  ) %>%
-#  fit(train_dl, epochs = n_epochs, valid_data = valid_dl)
-#
-#
-#
-#
-#
-#
-#
-#model <- net()
-#model$to(device = device)
-#
-#optimizer <- optim_adam(model$parameters)
-#
-## this will be called for every batch, see training loop below
-## loss <- nnf_cross_entropy(output, b[[2]]$to(device = device))
-#
-#
-#
-#for (epoch in 1:5) {
-#  
-#  l <- c()
-#  
-#  coro::loop(for (b in train_dl) {
-#    # make sure each batch's gradient updates are calculated from a fresh start
-#    optimizer$zero_grad()
-#    # get model predictions
-#    output <- model(b[[1]]$to(device = device))
-#    # calculate loss
-#    loss <- nnf_cross_entropy(output, b[[2]]$to(device = device))
-#    # calculate gradient
-#    loss$backward()
-#    # apply weight updates
-#    optimizer$step()
-#    # track losses
-#    l <- c(l, loss$item())
-#  })
-#  
-#  cat(sprintf("Loss at epoch %d: %3f\n", epoch, mean(l)))
-#}
-#
-#
-#
-#
-#
-## train_image_array_gen <- flow_images_from_directory(train_image_files_path, 
-##                                                     train_data_gen,
-##                                                     target_size = target_size,
-##                                                     class_mode = "categorical",
-##                                                     classes = animal_list,
-#                                                     seed = 42)
-# 
-# # validation images
-# valid_image_array_gen <- flow_images_from_directory(valid_image_files_path, 
-#                                                     valid_data_gen,
-#                                                     target_size = target_size,
-#                                                     class_mode = "categorical",
-#                                                     classes = animal_list,
-#                                                     seed = 42)
-
-
-## ----check generator---------------------------------------------------------
-# Check that things seem to have been read in OK
-# cat("Number of images per class:")
-# table(factor(train_image_array_gen$classes))
-# cat("Class labels vs index mapping")
-# train_image_array_gen$class_indices
-
-
-## ----final setup-------------------------------------------------------------
-# number of training samples
-# train_samples <- train_image_array_gen$n
-# # number of validation samples
-# valid_samples <- valid_image_array_gen$n
-
-# define batch size and number of epochs
-# batch_size <- 32 # Typical default, though possibly a little high given small dataset
-# epochs <- 10
-
-
-## ----define CNN structure----------------------------------------------------
-# initialise model
-# model <- keras_model_sequential()
-
-## add layers
-#model %>%
-#  # First convolutional layer
-#  layer_conv_2d(filter = 32, kernel_size = c(3,3),
-#                input_shape = c(img_width, img_height, channels),
-#                activation = "relu") %>%
-#
-#  # Second convolutional hidden layer
-#  layer_conv_2d(filter = 16, kernel_size = c(3,3),
-#                activation = "relu") %>%
-#
-#  # Use max pooling
-#  layer_max_pooling_2d(pool_size = c(2,2)) %>%
-#  layer_dropout(0.25) %>%
-#  
-#  # Flatten max filtered output into feature vector 
-#  # and feed into dense layer
-#  layer_flatten() %>%
-#  layer_dense(100, activation = "relu") %>%
-#  layer_dropout(0.5) %>%
-#  
-#  # Outputs from dense layer are projected onto output layer
-#  layer_dense(output_n, activation = "softmax") 
-#
-#
-### ----check CNN structure-----------------------------------------------------
-#print(model)
-#
-#
-### ----compile-----------------------------------------------------------------
-## Compile the model
-#model %>% compile(
-#  loss = "categorical_crossentropy",
-#  optimizer = optimizer_rmsprop(lr = 0.0001, decay = 1e-6),
-#  metrics = "accuracy"
-#)
-#
-#
-## ----train model, eval=FALSE--------------------------------------------------
-## Train the model with fit_generator
-#history <- model %>% fit_generator(
-#  # training data
-#  train_image_array_gen,
-#
-#  # epochs
-#  steps_per_epoch = as.integer(train_samples / batch_size),
-#  epochs = epochs,
-#
-#  # validation data
-#  validation_data = valid_image_array_gen,
-#  validation_steps = as.integer(valid_samples / batch_size),
-#
-#  # print progress
-#  verbose = 2
-#)
-#
-#
-## ----plot and save results, eval=FALSE----------------------------------------
-#plot(history)
-#save.image("animals.RData")
-#model %>% save_model_hdf5("animals_simple.hdf5")
-#
-#
-### ----augmented generator-----------------------------------------------------
-#train_data_gen = image_data_generator(
-#  rescale = 1/255 ,
-#  rotation_range = 40,
-#  width_shift_range = 0.2,
-#  height_shift_range = 0.2,
-#  shear_range = 0.2,
-#  zoom_range = 0.2,
-#  horizontal_flip = TRUE,
-#  fill_mode = "nearest"
-#)
-#train_image_array_gen <- flow_images_from_directory(train_image_files_path, 
-#                                                    train_data_gen,
-#                                                    target_size = target_size,
-#                                                    class_mode = "categorical",
-#                                                    classes = animal_list,
-#                                                    seed = 42)
-#
-#
-## ----fit augmented model, eval=FALSE------------------------------------------
-## Copy the original model (this is not essential; could keep the same name)
-#model_aug <- model
-#
-## Train the model with fit_generator
-#history_aug <- model %>% fit_generator(
-#  # training data
-#  train_image_array_gen,
-#
-#  # epochs
-#  steps_per_epoch = as.integer(train_samples / batch_size),
-#  epochs = epochs,
-#
-#  # validation data
-#  validation_data = valid_image_array_gen,
-#  validation_steps = as.integer(valid_samples / batch_size),
-#
-#  # print progress
-#  verbose = 2
-#)
-#
-#plot(history_aug)
-#
-#
-### ----one augmented image-----------------------------------------------------
-#fnames <- list.files(train_image_files_path, full.names = TRUE, recursive = TRUE)
-## Pick the third image (a butterfly) to look at
-#img_path <- fnames[[3]]                                               
-#
-#img <- image_load(img_path, target_size = c(150, 150))                
-#img_array <- image_to_array(img)                                      
-#img_array <- array_reshape(img_array, c(1, 150, 150, 3))              
-#
-## Very simple augmentation generator
-#augmentation_generator <- flow_images_from_data(                      
-#  img_array,                                                          
-#  generator = train_data_gen,                                                
-#  batch_size = 1
-#)
-#
-#op <- par(mfrow = c(2, 2), pty = "s", mar = c(1, 0, 1, 0))            
-#for (i in 1:4) {                                                      
-#  batch <- generator_next(augmentation_generator)                     
-#  plot(as.raster(batch[1,,,]))                                        
-#}                                                                     
-#par(op)                                                               
+joint %>%
+  mutate(Resample = 1) %>% 
+  group_by(Resample) %>%
+  roc_curve(truth, butterfly:spider) %>% 
+  autoplot()
